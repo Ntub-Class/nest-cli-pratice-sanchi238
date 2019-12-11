a58d5dd5d138019103870227efb0eb318ba402fd
Your Name <you@example.com>
2019-12-11 14:18:37 +0800

    n1086731

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+    @Get()
+    getUser() {
+        return 'YA';
+    }
+
+    @Get(':id')
+    getOneUser(@Param('id') id) {
+        return 'YA';
+    }
+
+    @Post()
+    postUser(@Body() body) {
+        return 'YA';
+    }
+
+    @Put(':id')
+    putUser(@Body() body, @Param('id') id) {
+        return 'YA';
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id) {
+        return 'YA';
+    }
+
+}
--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    @Get()
+    getUser() {
+        return 'Good';
+    }
+
+    @Get(':id')
+    getOneUser(@Param('id') id) {
+        return 'Good';
+    }
+
+    @Post()
+    postUser(@Body() body) {
+        return 'Good';
+    }
+
+    @Put(':id')
+    putUser(@Body() body, @Param('id') id) {
+        return 'Good';
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id) {
+        return 'Good';
+    }
+
+}
